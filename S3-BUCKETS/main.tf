98dbaeda974c054a6f69520d667dd71c0428ee56
NextGen20 <amitbachar2@gmail.com>
2023-06-06 17:50:30 +0300

    push

---
+++ S3-BUCKETS/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "project-3" {
+   count = length(var.ec2_instance)
+   ami = "ami-0261755bbcb8c4a84"
+   instance_type = var.choose_type[count.index]
+   user_data =  <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y terraform
+    apt install default-jre
+
+  EOF
+    tags = {
+    Name = var.ec2_instance[count.index]
+  }
+}
---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "ec2_instance" {
+  type = list(string)
+  default = ["Prod", "Test", "Slave", "second-Prod"]
+}
+
+variable "choose_type" {
+  type = list(string)
+  default = [ "t2.micro", "t2.small", "t2.large" ]
+}
