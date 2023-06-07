5ab3c4d3d9c097874f58d80bde63ff635e0ffbe4
NextGen20 <amitbachar2@gmail.com>
2023-06-07 18:45:25 +0300

    a

--- EC2-INSTANCES/main.tf
+++ EC2-INSTANCES/main.tf
@@ -12,7 +12,8 @@ variable "choose_type" {
 resource "aws_instance" "project-3" {
   count         = length(var.ec2_instance)
   ami           = "ami-0261755bbcb8c4a84"
-  instance_type = "t2.micro"
+  # instance_type = "t2.micro"
+  instance_type = var.choose_type[0]
   user_data     = <<-EOF
     #!/bin/bash
     apt-get update
