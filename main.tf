9d426a02ca1c0a01f406a6ad896ff8c526b24e68
NextGen20 <amitbachar2@gmail.com>
2023-06-07 16:26:53 +0300

    PUSH

--- main.tf
+++ main.tf
@@ -5,19 +5,17 @@ provider "aws" {
 module "ec2" {
   source = "./EC2-INSTANCES"
 }
-# module "agents" {
-#   source = "./modules/agents-module"
-# }
+
 module "s3" {
   source = "./S3-BUCKETS"
 }
 
 module "iam" {
-  source = "./modules/iam-module"
+  source = "./IAM"
 }
 
 module "dynamodb" {
-  source = "./modules/dynamodb-module"
+  source = "./DYNAMODB"
 }
 
 
