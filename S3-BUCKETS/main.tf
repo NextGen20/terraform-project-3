9f7af76d4b951081eb1be532f09061f4104fd5e3
NextGen20 <amitbachar2@gmail.com>
2023-06-07 16:46:56 +0300

    s

--- S3-BUCKETS/main.tf
+++ S3-BUCKETS/main.tf
@@ -1,15 +1,9 @@
-resource "aws_instance" "project-3" {
-   count = length(var.ec2_instance)
-   ami = "ami-0261755bbcb8c4a84"
-   instance_type = var.choose_type[count.index]
-   user_data =  <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install -y terraform
-    apt install default-jre
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-project3-bucket"
+  acl    = "public-read"
 
-  EOF
-    tags = {
-    Name = var.ec2_instance[count.index]
+  tags = {
+    Name        = "my-project3-bucket"
+    Environment = "Production"
   }
 }
