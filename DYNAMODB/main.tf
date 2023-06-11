73b3f6e870ff30b6ee7183fc66ae1bc26c172438
NextGen20 <amitbachar2@gmail.com>
2023-06-11 14:46:11 +0300

    s

--- DYNAMODB/main.tf
+++ DYNAMODB/main.tf
@@ -1,16 +1,29 @@
-resource "aws_dynamodb_table" "example" {
-  name           = "example_table"
- 
-  read_capacity  = 5
-  write_capacity = 5
-
+resource "aws_dynamodb_table" "book_catalog_table" {
+  name             = "BookCatalog"
+  billing_mode     = "PROVISIONED"
+  read_capacity    = 1
+  write_capacity   = 1 
+  hash_key         = "BookName"
+  
   attribute {
-    name = "id"
-    type = "N"
+    name = "BookName"
+    type = "S"
   }
   
-  key {
-    name = "id"
-    type = "HASH"
+  attribute {
+    name = "Author"
+    type = "S"
+  }
+  global_secondary_index {
+    name               = "Author-Index"
+    hash_key           = "Author"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["Genre"]
+  }
+  tags = {
+    Name        = "book-catalog-table"
+    Environment = "production"
   }
 }
