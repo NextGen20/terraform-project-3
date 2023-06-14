59312fa676ad0ed04e99efaaca133c639ca46879
NextGen20 <amitbachar2@gmail.com>
2023-06-14 12:22:04 +0300

    changes

--- GITHUB/main.tf
+++ GITHUB/main.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 provider "github" {
-  token        = "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"
-  organization = "Bachar and his sons"
+  token        = "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"
+  # organization = "Bachar and his sons"
 }
 
 resource "github_repository" "example" {
   name        = "project-3"
   description = "Example repository"
-  private     = false
+  visibility     = "public"
   has_issues  = true
   has_projects = true
   has_wiki    = true
